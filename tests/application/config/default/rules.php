910cd17f253e618cd03693435c6ec09132afb56d
snail <arraykeys@gmail.com>
2015-04-29 18:15:59 +0800

    Signed-off-by: snail <arraykeys@gmail.com>

--- tests/application/config/default/rules.php
+++ tests/application/config/default/rules.php
@@ -1,6 +1,8 @@
 <?php
 
-//验证规则返回布尔代表成功和失败，返回null和其它值代表不设置和设置值
+//验证规则返回布尔代表成功和失败
+//如果要改变传入的值，把改变后的值赋值给$returnValue变量即可
+//对于一个字段多条验证规则存在的时候，如果设置$break为true那么该字段的该条规则之后的验证规则不在起作用
 
 return array(
     'default' => function($key, $value, $data, $args, &$returnValue = null, &$break = false) {
