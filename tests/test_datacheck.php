af5ce0e7f26f80b6dc7ea563c67472c587d6e3d3
snail <arraykeys@gmail.com>
2015-05-06 14:07:45 +0800

    Signed-off-by: snail <arraykeys@gmail.com>

---
+++ tests/test_datacheck.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once 'pluginfortest.php';
+//require_once('simpletest/web_tester.php');
+require_once('simpletest/autorun.php');
+
+//require_once('simpletest/browser.php');
+/**
+ * Soter测试案例
+ * Test by snail（672308444@163.com）
+ */
+class TestDataCheck extends UnitTestCase {
+
+	public function testArray() {
+		
+	}
+
+}
